dc2f5cbb4201f302826b01d6a7c3fffc32864574
Matheus Victor <matheusvictor.salles@gmail.com>
2022-02-17 08:39:08 -0300

    Add lista de contatos na tela principal
    A recyclerView será inflada na MainActivity.kt com lista de contatos mockada

--- digital-innovation-one/ContactsApp/app/src/main/java/com/matheusvictor/dio/mycontacts/MainActivity.kt
+++ digital-innovation-one/ContactsApp/app/src/main/java/com/matheusvictor/dio/mycontacts/MainActivity.kt
@@ -2,10 +2,40 @@ package com.matheusvictor.dio.mycontacts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.matheusvictor.dio.mycontacts.adapter.ContactAdapter
+import com.matheusvictor.dio.mycontacts.model.Contact
 
 class MainActivity : AppCompatActivity() {
+
+    private val rvList: RecyclerView by lazy {
+        findViewById(R.id.rv_list)
+    }
+
+    private val adapter = ContactAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bindView()
+        updateList()
+
     }
+
+    private fun bindView() {
+        rvList.adapter = adapter
+        rvList.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun updateList() {
+        adapter.updateList(
+            arrayListOf(
+                Contact("Matheus Victor", "(00) 0000-0000", "img.png"),
+                Contact("Matheus Victor", "(00) 0000-0000", "img.png")
+            )
+        )
+    }
+
 }
