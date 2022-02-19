2771e79403babaec86df2c5b81f0bf7857fa5ac0
Matheus Victor <matheusvictor.salles@gmail.com>
2022-02-19 11:01:08 -0300

    Implementa toolbar e botão para voltar à Activity principal
    Como o tema adota foi personalizado e não implementa automaticamente as toolbars, foi criada a exibição de uma toolbar em activity_contact_detail.xml e na ContactDetailActivity.kt foi implementado dois métodos: um para instanciar a toolbar na activity junto com um botão de voltar, e o outro método responsável por dar o comportamento de fechar a ContactDetailActivity.kt quando o botão de voltar for clicado

--- digital-innovation-one/ContactsApp/app/src/main/java/com/matheusvictor/dio/mycontacts/ContactDetailActivity.kt
+++ digital-innovation-one/ContactsApp/app/src/main/java/com/matheusvictor/dio/mycontacts/ContactDetailActivity.kt
@@ -3,6 +3,8 @@ package com.matheusvictor.dio.mycontacts
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
 import com.matheusvictor.dio.mycontacts.model.Contact
 
 class ContactDetailActivity : AppCompatActivity() {
@@ -13,6 +15,7 @@ class ContactDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact_detail)
 
+        initToolbar()
         getExtras()
         bindViews()
     }
@@ -24,6 +27,17 @@ class ContactDetailActivity : AppCompatActivity() {
     private fun bindViews() {
         findViewById<TextView>(R.id.tv_name).text = contact?.name
         findViewById<TextView>(R.id.tv_phone).text = contact?.phone
+    }
+
+    private fun initToolbar() {
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return true
     }
 
     companion object {
