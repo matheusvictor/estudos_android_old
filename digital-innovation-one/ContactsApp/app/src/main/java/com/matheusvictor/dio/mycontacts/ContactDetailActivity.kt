02224c8f701732bdabbc743a1fe346612a2bc3dd
Matheus Victor <matheusvictor.salles@gmail.com>
2022-02-19 10:46:01 -0300

    Implementa parcialização da classe
    A implementa da interface Parcelable permite escrever os dados que julgamos necessário de uma Classe em um objeto Parcel, que é a entidade que será transmitida para outra Activity

--- digital-innovation-one/ContactsApp/app/src/main/java/com/matheusvictor/dio/mycontacts/ContactDetailActivity.kt
+++ digital-innovation-one/ContactsApp/app/src/main/java/com/matheusvictor/dio/mycontacts/ContactDetailActivity.kt
@@ -2,10 +2,32 @@ package com.matheusvictor.dio.mycontacts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.matheusvictor.dio.mycontacts.model.Contact
 
 class ContactDetailActivity : AppCompatActivity() {
+
+    private var contact: Contact? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact_detail)
+
+        getExtras()
+        bindViews()
     }
+
+    private fun getExtras() {
+        contact = intent.getParcelableExtra(EXTRA_CONTACT)
+    }
+
+    private fun bindViews() {
+        findViewById<TextView>(R.id.tv_name).text = contact?.name
+        findViewById<TextView>(R.id.tv_phone).text = contact?.phone
+    }
+
+    companion object {
+        const val EXTRA_CONTACT: String = "EXTRA_CONTACT"
+    }
+
 }
