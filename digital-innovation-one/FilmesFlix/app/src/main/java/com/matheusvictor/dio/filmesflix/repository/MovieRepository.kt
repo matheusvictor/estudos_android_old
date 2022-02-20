a0ecc011c3c922c797e2e6398ede50f3db2cfca3
Matheus Victor <matheusvictor.salles@gmail.com>
2022-02-20 11:54:58 -0300

    Implementa camada de repositório

---
+++ digital-innovation-one/FilmesFlix/app/src/main/java/com/matheusvictor/dio/filmesflix/repository/MovieRepository.kt
@@ -0,0 +1,31 @@
+package com.matheusvictor.dio.filmesflix.repository
+
+import android.util.Log
+import com.matheusvictor.dio.filmesflix.api.MovieRestApiTask
+import com.matheusvictor.dio.filmesflix.model.Movie
+
+class MovieRepository(private val movieRestApiTask: MovieRestApiTask) {
+
+    companion object {
+        const val TAG = "MovieRepository"
+    }
+
+    private val movieList = arrayListOf<Movie>()
+
+    fun getAllMovies(): List<Movie> {
+        val request = movieRestApiTask.retrofitApi().getAllMovies().execute()
+
+        if (request.isSuccessful) {
+            request.body()?.let {
+                movieList.addAll(it)
+            }
+        } else {
+            request.errorBody()?.let {
+                Log.d(TAG, it.toString())
+            }
+        }
+
+        return movieList
+    }
+
+}
