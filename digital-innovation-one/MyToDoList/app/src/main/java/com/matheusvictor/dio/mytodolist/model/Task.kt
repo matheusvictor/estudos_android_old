9c0faa4ebde2f474920a24b2655aca2649fb77e9
Matheus Victor <matheusvictor.salles@gmail.com>
2022-02-20 23:39:15 -0300

    Cria modelo de classe

---
+++ digital-innovation-one/MyToDoList/app/src/main/java/com/matheusvictor/dio/mytodolist/model/Task.kt
@@ -0,0 +1,9 @@
+package com.matheusvictor.dio.mytodolist.model
+
+data class Task(
+    val id: Int = 0,
+    val title: String,
+    val description: String?,
+    val date: String,
+    val hour: String
+)
