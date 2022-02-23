9dadf391d2331fadace147de74e8ed27152ca9d8
Matheus Victor <matheusvictor.salles@gmail.com>
2022-02-22 23:10:06 -0300

    Implementa comportamento de estado vazio
    Quando não houver nenhuma tarefa na lista, a aplicação apresentará uma imagem de estado vazio.

--- digital-innovation-one/MyToDoList/app/src/main/java/com/matheusvictor/dio/mytodolist/ui/MainActivity.kt
+++ digital-innovation-one/MyToDoList/app/src/main/java/com/matheusvictor/dio/mytodolist/ui/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.matheusvictor.dio.mytodolist.databinding.ActivityMainBinding
 import com.matheusvictor.dio.mytodolist.datasource.TaskDataSource
 import com.matheusvictor.dio.mytodolist.model.Task
@@ -50,7 +51,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun updateList() {
-        adapter.submitList(TaskDataSource.getList())
+        val list = TaskDataSource.getList()
+
+        binding.imageEmptyState.emptyState.visibility = if (list.isEmpty())
+            View.VISIBLE
+        else
+            View.GONE
+
+        adapter.submitList(list)
     }
 
     companion object {
